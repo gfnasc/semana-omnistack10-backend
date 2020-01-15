08caa4a4d08ff01dcb04017db2913279fe0dd212
Guilherme Nascimento <guilherme.freitasnascimento@gmail.com>
2020-01-15 01:39:05 -0200

    criação da listagem geral de devs, criação da busca de devs e filtros do mongodb

--- src/controllers/DevController.js
+++ src/controllers/DevController.js
@@ -1,7 +1,14 @@
 const axios = require('axios');
 const Dev = require('../models/Dev');
+const parseStringAsArray = require('./utils/parseStringAsArray');
 
 module.exports = {
+
+    async index(req, res){
+        const devs = await Dev.find();
+        return res.json(devs);
+    },
+
     async store(req, res) {
         const { github_username, techs, latitude, longitude } = req.body;
 
@@ -11,14 +18,14 @@ module.exports = {
             const apiResponse = await axios.get(`https://api.github.com/users/${github_username}`);
             const { name = login, avatar_url, bio } = apiResponse.data;
 
-            const techsArray = techs.split(',').map(tech => tech.trim());
+            const techsArray = parseStringAsArray(techs);
 
             const location = {
                 type: 'Point',
                 coordinates: [longitude, latitude]
             }
 
-            const dev = await Dev.create({
+            dev = await Dev.create({
                 github_username,
                 name,
                 avatar_url,
