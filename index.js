1489e4ed8357c4526472c4c1d980e95f5b92dfb5
Guilherme Nascimento <guilherme.freitasnascimento@gmail.com>
2020-01-14 23:39:36 -0200

    configurando o banco de dados

--- index.js
+++ index.js
@@ -1,6 +1,14 @@
 const express = require('express')
+const mongoose = require('mongoose');
 
 const app = express();
+
+mongoose.connect('mongodb+srv://gfnasc:0jaq13YF@cluster0-qqyhy.mongodb.net/test?retryWrites=true&w=majority',{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
+
+app.use(express.json());
 
 app.get('/', (req, res) => {
     return res.json({   
