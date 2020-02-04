c83c2da680589b967623717992e056d14dcfa0de
Guilherme Nascimento <guilherme.freitasnascimento@gmail.com>
2020-02-04 11:50:26 -0300

    correção cors

--- src/index.js
+++ src/index.js
@@ -5,12 +5,12 @@ const routes = require('./routes')
 
 const app = express();
 
-mongoose.connect('mongodb+srv://gfnasc:0jaq13YF@cluster0-qqyhy.mongodb.net/test?retryWrites=true&w=majority',{
+mongoose.connect('mongodb+srv://gfnasc:0jaq13YF@cluster0-qqyhy.gcp.mongodb.net/test?retryWrites=true&w=majority',{
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
 
-app.use(cors);
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
